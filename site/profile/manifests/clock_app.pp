fdf26925d535566ff85c620924c7f6831c0b068c
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2019-03-20 15:50:10 +0000

    Moved resource

--- site/profile/manifests/clock_app.pp
+++ site/profile/manifests/clock_app.pp
@@ -1,19 +1,25 @@
 class profile::clock_app (
-
+  String $docroot = '/var/www'
 ) {
-  file { '/var/www':
+  file { $docroot:
     ensure => directory,
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
   }
 
-  file { "/var/www/index.html":
+  file { "${docroot}/index.html":
     ensure => file,
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
     source => 'puppet:///modules/profile/clock.html',
   }
+
+  # Set up the server resource for the clock host
+  nginx::resource::server { $facts['fqdn']:
+    listen_port => 8080,
+    www_root    => $docroot,
+  } 
 
 }
